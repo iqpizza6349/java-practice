534eaf626c8713e7908c948041c5660aca190f75
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-26 20:57:45 +0900

    백준 ATM

--- src/log/iqpizza6349/Main.java
+++ src/log/iqpizza6349/Main.java
@@ -1,11 +1,12 @@
 package log.iqpizza6349;
 
+import log.iqpizza6349.greedy.ATM;
 import log.iqpizza6349.greedy.SugarDelivery;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution<?> solution = new SugarDelivery();
+        Solution<?> solution = new ATM();
         System.out.println(solution.solution());
     }
 }
---
+++ src/log/iqpizza6349/greedy/ATM.java
@@ -0,0 +1,41 @@
+package log.iqpizza6349.greedy;
+
+import log.iqpizza6349.Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * <a href="https://www.acmicpc.net/problem/11399">백준 온라인 저지 11399 번 ATM</a>
+ */
+public class ATM implements Solution<Integer> {
+
+    @Override
+    public Integer solution() {
+        try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int[] times = new int[stringTokenizer.countTokens()];
+            int i = 0;
+            while (stringTokenizer.hasMoreTokens()) {
+                times[i++] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+            int sum = 0;
+            Arrays.sort(times);
+            for (int j = 0; j < i; j++) {
+                int wait = 0;
+                for (int k = j; k >= 0; k--) {
+                    wait += times[k];
+                }
+                sum += wait;
+            }
+
+            return sum;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
