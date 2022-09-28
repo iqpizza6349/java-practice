76191dd15d42bf7ec1f80c1c955042c517fc7c99
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-28 20:33:28 +0900

    백준 회의실 배정

--- src/log/iqpizza6349/Main.java
+++ src/log/iqpizza6349/Main.java
@@ -1,11 +1,14 @@
 package log.iqpizza6349;
 
-import log.iqpizza6349.greedy.Coin0;
+import log.iqpizza6349.greedy.MeetingRoom;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution<?> solution = new Coin0();
+        Solution<?> solution = new MeetingRoom();
         System.out.println(solution.solution());
+        Runtime.getRuntime().gc();
+        long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.print(usedMemory + " bytes");
     }
 }
---
+++ src/log/iqpizza6349/greedy/MeetingRoom.java
@@ -0,0 +1,50 @@
+package log.iqpizza6349.greedy;
+
+import log.iqpizza6349.Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * <a href="https://www.acmicpc.net/problem/1931">백준 온라인 저지 1931 번 회의실 배정</a>
+ */
+public class MeetingRoom implements Solution<Integer> {
+
+    @Override
+    public Integer solution() {
+        try (final BufferedReader bufferedReader
+                     = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int[][] rooms = new int[N][2];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                rooms[i][0] = Integer.parseInt(stringTokenizer.nextToken());
+                rooms[i][1] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            Arrays.sort(rooms, (o1, o2) -> {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
+            });
+
+            int maxCount = 0;
+            int prevEndTime = 0;
+            for (int i = 0; i < N; i++) {
+                if (prevEndTime <= rooms[i][0]) {
+                    prevEndTime = rooms[i][1];
+                    maxCount++;
+                }
+            }
+
+            return maxCount;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
