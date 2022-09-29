46b3cb70ffaa00be74f75668731e84a9d2d4a18b
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-29 09:16:42 +0900

    백준 보물

--- src/log/iqpizza6349/Main.java
+++ src/log/iqpizza6349/Main.java
@@ -1,14 +1,14 @@
 package log.iqpizza6349;
 
-import log.iqpizza6349.greedy.MeetingRoom;
+import log.iqpizza6349.greedy.Treasure;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution<?> solution = new MeetingRoom();
+        Solution<?> solution = new Treasure();
         System.out.println(solution.solution());
         Runtime.getRuntime().gc();
         long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        System.out.print(usedMemory + " bytes");
+        System.out.print((usedMemory / 1024) + " kb");
     }
 }
---
+++ src/log/iqpizza6349/greedy/Treasure.java
@@ -0,0 +1,44 @@
+package log.iqpizza6349.greedy;
+
+import log.iqpizza6349.Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/**
+ * <a href="https://www.acmicpc.net/problem/1026">백준 온라인 저지 1026번 보물</a>
+ */
+public class Treasure implements Solution<Integer> {
+
+    @Override
+    public Integer solution() {
+        try (final BufferedReader bufferedReader
+                     = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int[][] AB = new int[2][N];
+            for (int i = 0; i < 2; i++) {
+                int j = 0;
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                while (stringTokenizer.hasMoreTokens()) {
+                    AB[i][j++] = Integer.parseInt(stringTokenizer.nextToken());
+                }
+            }
+
+            Arrays.sort(AB[0]);
+            Integer[] copyB = Arrays.stream(AB[1]).boxed().toArray(Integer[]::new);
+            Arrays.sort(copyB, Collections.reverseOrder());
+
+            int S = 0;
+            for (int i = 0; i < N; i++) {
+                S += (AB[0][i] * copyB[i]);
+            }
+            return S;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
